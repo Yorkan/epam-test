e2b305df05c119c9a8813026b488dc2a1171e630
Aleks Kol <kol.aleksandr@gmail.com>
2022-09-10 16:12:30 +0300

    Added first script file

---
+++ case_statet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $1 in
+help) echo  "You can use start|stop|restart|help for service" ;;
+start) sleep 600 & echo  "Service started";;
+    #var=$!
+    #var='pgrep sleep | awk '{print $!}''
+    #echo $var 
+    #ps ax | grep sleep
+    #kill -9 $var;;
+stop) ps aux |grep [s]leep| kill -9 $(awk '{print $2}')
+echo "Service stopped" ;;
+restart) ps aux |grep [s]leep| kill -9 $(awk '{print $2}')
+    echo "Service stopped"
+    sleep 100 &
+    echo "Service started"
+;;
+*) echo "Add parameter: start|stop|restart|help" ;;
+esac
